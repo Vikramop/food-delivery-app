45b58b1b4de2434320875aeed68692c4f1a537e3
Veervikram singh <veervikramsingh132@gmail.com>
2023-03-20 08:54:39 +0530

    added navbar and footer

---
+++ components/Footer.jsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" objectFit="cover" layout="fill" alt="" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            OH YES, WE DID.THE PIZZA HUT, WELL BAKED SLICE OF PIZZA.
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+          <p className={styles.text}>
+            Veerapuram, Mahindra city.
+            <br /> Chennai, 85022
+            <br /> 98745 21459
+          </p>
+          <p className={styles.text}>
+            Chengalpattu.
+            <br /> Chennai, 85022
+            <br /> 98745 21459
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+          <p className={styles.text}>
+            MONDAY UNTIL FRIDAY
+            <br /> 9:00 – 22:00
+          </p>
+          <p className={styles.text}>
+            SATURDAY - SUNDAY
+            <br /> 12:00 – 24:00
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
